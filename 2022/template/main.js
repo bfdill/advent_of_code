0970188baf2616e8671200d39d566f52545d0ea5
bfd <ben.dill@kroger.com>
2022-12-01 21:11:06 -0500

    chore(day 2): prep

---
+++ 2022/template/main.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+import { readFile } from 'node:fs/promises'
+
+const processData = data => {}
+
+const main = async filename => {
+  const data = await readFile(filename, { encoding: 'utf8' })
+
+  processData(data.split(/\n/))
+}
+
+await main(process.argv.slice(2)[0])
