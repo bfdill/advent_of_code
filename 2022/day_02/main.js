acc24c96d13a8f97a69da55e7e9ba51432362a32
bfd <ben.dill@kroger.com>
2022-12-02 15:10:24 -0500

    feat(day 2): part 1

--- 2022/day_02/main.js
+++ 2022/day_02/main.js
@@ -2,7 +2,43 @@
 
 import { readFile } from 'node:fs/promises'
 
-const processData = data => {}
+const hands = {
+  A: 'rock',
+  B: 'paper',
+  C: 'scissors',
+  X: 'rock',
+  Y: 'paper',
+  Z: 'scissors'
+}
+
+const outcomes = {
+  AX: 'draw',
+  AY: 'win',
+  AZ: 'loss',
+  BX: 'loss',
+  BY: 'draw',
+  BZ: 'win',
+  CX: 'win',
+  CY: 'loss',
+  CZ: 'draw'
+}
+
+const scores = {
+  rock: 1,
+  paper: 2,
+  scissors: 3,
+  lost: 0,
+  draw: 3,
+  win: 6
+}
+
+const processData = data => {
+  const result = data
+    .map(x => x[0] + x[2])
+    .map(x => (outcomes[x] === 'loss' ? scores[hands[x[1]]] : scores[outcomes[x]] + scores[hands[x[1]]]))
+    .reduce((pv, cv) => pv + cv, 0)
+  console.log(result)
+}
 
 const main = async filename => {
   const data = await readFile(filename, { encoding: 'utf8' })
