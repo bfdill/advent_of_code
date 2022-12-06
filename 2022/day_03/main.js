53fc32ed2139e115ca1dabcb234d304b31dd126e
bfd <ben.dill@kroger.com>
2022-12-05 22:16:25 -0500

    feat(day 3): part 2

--- 2022/day_03/main.js
+++ 2022/day_03/main.js
@@ -13,21 +13,43 @@ class Rucksack {
     this.compartment2 = supplies.slice(supplies.length / 2, supplies.length)
   }
 
-  findDuplicate = () =>
-    this.compartment1.split('').reduce((pv, cv, i, a) => {
-      if (this.compartment2.includes(cv)) {
-        a.length = 0
-        return cv
-      }
-    }, '')
+  _findDuplicate = (first, second) => {
+    const a = typeof first === 'string' ? new Set(first.split('')) : new Set(first),
+      b = new Set(second.split(''))
 
-  getPriority = () => scoredAlphabet.indexOf(this.findDuplicate())
+    return [...a].filter(x => b.has(x))
+  }
+
+  findDuplicate = () => this._findDuplicate(this.compartment1, this.compartment2)
+
+  getPriority = () => scoredAlphabet.indexOf(this.findDuplicate().at(0))
+}
+
+class ThreePackRucksack extends Rucksack {
+  constructor(first, second, third) {
+    super(first)
+
+    this.first = first
+    this.second = second
+    this.third = third
+  }
+
+  findDuplicate = () => this._findDuplicate(this._findDuplicate(this.first, this.second), this.third)
 }
 
 const processData = data => {
-  const result = data.map(x => new Rucksack(x).getPriority()).reduce((pv, cv) => pv + cv, 0)
+  console.log({
+    results: data.map(x => new Rucksack(x).getPriority()).reduce((pv, cv) => pv + cv, 0),
+    records: data.length
+  })
 
-  console.log({ result, records: data.length })
+  let total = 0
+
+  for (let i = 0; i < data.length - 2; i += 3) {
+    total += new ThreePackRucksack(data[i], data[i + 1], data[i + 2]).getPriority()
+  }
+
+  console.log({ total, records: data.length })
 }
 
 const main = async filename => {
